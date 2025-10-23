73f5a01931c4d6d8ff0b97c09e92d83641f3d2cd
ajaykedar123 <ajaykedar3790@gmail.com>
2025-10-23 13:30:56 +0530

    NEW API LINK UPDATE

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500, // increase limit from default 500 KB
+  },
 })
